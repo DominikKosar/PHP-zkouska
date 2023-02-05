f5d63ef7d88f8c3257d7be70981badf452d1b244
Dominik Košař <dom707@seznam.cz>
2023-02-05 12:16:50 +0100

    Kontrola délky uživatelského jména a hlášky

--- formular.php
+++ formular.php
@@ -1,12 +1,24 @@
 <?php
 
     if (isset($_POST["submit"])){
+
+        $minimum = 3;
+        $maximum = 10;
         $username = $_POST["username"];
         $password = $_POST["password"];
 
         echo "Ahoj " . $username;
         echo "<br>";
         echo "Tvé heslo je: " . $password;
+        echo "<br>";
+
+        if(strlen($username) <= $minimum){
+            echo "Uživatelské jméno musí být delší než 3 znaky";
+        }
+
+        if(strlen($username) >= $maximum){
+            echo "Uživatelské jméno musí být kratší než 10 znaků";
+        }
     }
 
 ?>
