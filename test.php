4f4a78dad7199fd65019032a0dbeee13e5b2cdb3
Dominik Košař <dom707@seznam.cz>
2023-04-07 15:09:04 +0200

    ochrana hesel -> hashování hesel v test.php

--- test.php
+++ test.php
@@ -9,8 +9,21 @@
 <body>
     <!-- SQL injection -->
     <?php
+    /*
     echo 'O\'Reilly';
-    echo "<a href=\"#odkaz\">Text odkazu</a>";
+    echo "<a href=\"#odkaz\">Text odkazu</a>";*/
+
+    //Hashování hesel
+    //https://www.php.net/manual/en/function.crypt.php --> odkaz na hashování hesel
+    $password = "123456";
+
+    $hashFormat = "$2y$10$";     //použij bloufish a projeď ho 10krát
+    //$salt = "blaznivafunkcenahesla9";       //22 znaků musí být
+    $salt = "u9YPT1kh13fEPGlMmkWrID";
+    $hashFormat_salt = $hashFormat.$salt;   //spojení mezi $hashFormat a $salt
+
+    $password = crypt($password, $hashFormat_salt);
+    echo $password;
 
     ?>
     
